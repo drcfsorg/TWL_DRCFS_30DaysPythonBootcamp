15e590f69eba93f8809b91f42ad06382ea7550a3
YOUR NAME <EMAIL>
2022-12-08 20:05:13 +0545

    dec8

---
+++ Week 1/Personal/fizzbuzz.py
@@ -0,0 +1,13 @@
+# Program details
+# divisible by 3, print fizz
+# divisible by 5, print Buzz
+# divisible by both 3 and 5, Fizznuzz
+
+
+for i in range(100):
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 3 == 0:
+        print("Buzz")
+    else:
+        print(i)
