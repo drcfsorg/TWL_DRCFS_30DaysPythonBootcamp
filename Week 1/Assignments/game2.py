07660f17e051d6c1ce62c15d7426db5d2011c655
YOUR NAME <EMAIL>
2022-12-09 19:34:27 +0545

    game2

---
+++ Week 1/Assignments/game2.py
@@ -0,0 +1,18 @@
+import random
+user_action = input("Enter a choice(rock, paper, scissors):")
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+if user_action == computer_action:
+    print("Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("You win")
+    else:
+        print("paper covers rock! you lose ")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers the rock, You win")
+    else:
+        print("Scissors cut paper! you lose")
+
+
