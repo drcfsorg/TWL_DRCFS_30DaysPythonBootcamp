a5396ccab84434127ca7e25e25af64211c43b17b
CaffeineDuck <hello@samrid.me>
2022-12-11 14:39:52 +0545

    teach about python modules

---
+++ Week 2/lectures/workshop.py
@@ -0,0 +1,13 @@
+# Import the module directly
+import constants
+
+# Import variables from constant
+from constants import CURRENT_MENTOR_COUNT
+
+# Teach about * in imports
+
+# Printing using module
+print('There are currently', constants.CURRENT_STUDENT_COUNT, 'number of participants')
+
+# Priting by directing import variable from module
+print('There are currently ' +  str(CURRENT_MENTOR_COUNT) +  ' number of mentors')
