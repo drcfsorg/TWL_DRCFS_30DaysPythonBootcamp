55dc44e74c997bc3dd20e8110d4cefb6c401ced9
Ashim Dahal <codeashim@gmail.com>
2022-12-13 17:54:19 +0545

    feat: assignment for file handling class

---
+++ Week 2/Assignments/filehandling.py
@@ -0,0 +1,12 @@
+'''
+Write a Python function called read_file() that takes a single parameter
+filename, and opens the specified file in read-only mode. 
+The function should read the entire contents of the file and return the contents as a string.
+'''
+
+def read_file(filename):
+    # TODO: Open the file in read-only mode and read its contents. Return the contents of the file.
+    pass
+
+contents = read_file("my_file.txt")
+print(contents)  # Output: "Hello, world!\nI just did my first Assignment of week 2."
